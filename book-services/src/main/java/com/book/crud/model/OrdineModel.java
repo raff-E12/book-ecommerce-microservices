99a756325d30c02e022ed501b3272fcd05728da0
Raff_E12 <raffaelecoppola381@gmail.com>
2026-04-15 16:15:20 +0200

    feat: Modifica con JPA...

--- book-services/src/main/java/com/book/crud/model/OrdineModel.java
+++ book-services/src/main/java/com/book/crud/model/OrdineModel.java
@@ -7,7 +7,7 @@ import java.math.BigDecimal;
 import java.util.List;
 
 @Entity
-@Table(name = "ordini",  schema = "privato", catalog = "book-api-dispence")
+@Table(name = "ordini")
 public class OrdineModel implements Serializable {
 
     @Id
